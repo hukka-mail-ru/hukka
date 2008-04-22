9b706a340355648485d399c4a214be4b7b096fae
hukka@mail.ru <hukka@mail.ru@65cfae95-534b-0410-ba89-db903692e17a>
2008-04-22 06:41:06 +0000

    'color' module added

---
+++ trunk/color/color.cpp
@@ -0,0 +1,16 @@
+#include <color.hpp>
+
+using namespace std;
+
+string color(int color)
+{
+	char command[13];
+
+    if(color != UNDEFINED)
+        sprintf(command, "%c[%d;%d;%dm", 0x1B, BRIGHT, color + 30, BLACK + 40);
+    else
+        sprintf(command, "%c[%d;%d;%dm", 0x1B, RESET, WHITE + 30, BLACK + 40);
+
+    std::string s = command;
+    return s;
+}
