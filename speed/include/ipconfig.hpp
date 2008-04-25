3d3eacd43e0f1b6e7ef60bfb46cceab8c8990bde
hukka <hukka@mail.ru>
2008-04-25 09:01:09 +0000

    Network interfaces finding

--- speed/include/ipconfig.hpp
+++ speed/include/ipconfig.hpp
@@ -1,24 +1,36 @@
 #ifndef __IPCONFIG_HPP__
 #define __IPCONFIG_HPP__
 
+#include <vector>
 #include <string>
 #include <iostream>
+
+#include <boost/shared_ptr.hpp>
+
+struct Interface
+{
+    std::string mName;
+    std::string mAddress;
+    std::string mMask;
+    std::string mMacAddress;
+};
+
+
 
 class IPConfig
 {
     public:
 
-	int queryInterface(const char* interface);
+	int queryInterfaces();
 	
+	/*
 	const std::string& getAddress()    { return mAddress; }
 	const std::string& getMask()       { return mMask; }
 	const std::string& getMacAddress() { return mMacAddress; }
-
+*/
     private:
 
-	std::string mAddress;
-	std::string mMask;
-	std::string mMacAddress;
+	std::vector<boost::shared_ptr<Interface> > mInterfaces;
 
 };
 
