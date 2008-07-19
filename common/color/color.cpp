d5e1b68016b23cf00718a2ed6db118cb6a87adc9
hukka <hukka@mail.ru>
2008-07-19 17:17:18 +0000

    color moved

---
+++ common/color/color.cpp
@@ -0,0 +1,21 @@
+#include <color.hpp>
+
+using namespace std;
+
+bool EnableColorLog = false;
+
+string color(int color)
+{
+    if(!EnableColorLog)
+        return ""; 
+
+	char command[13];
+
+    if(color != UNDEFINED)
+        sprintf(command, "%c[%d;%d;%dm", 0x1B, BRIGHT, color + 30, BLACK + 40);
+    else
+        sprintf(command, "%c[%d;%d;%dm", 0x1B, RESET, WHITE + 30, BLACK + 40);
+
+    std::string s = command;
+    return s;
+}
