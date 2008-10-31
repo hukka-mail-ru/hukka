be818ea094975d3718e407158b1f1476c64ebeed
hukka@mail.ru <hukka@mail.ru@65cfae95-534b-0410-ba89-db903692e17a>
2008-10-31 15:26:31 +0000

    hello fixed to allocate random driver number

--- trunk/driver/restart.sh
+++ trunk/driver/restart.sh
@@ -1,24 +1,28 @@
-major=70
-minor=0
+module=hello
 
 # remove module
-echo "Remove 'hello' module..."
-sudo /sbin/rmmod hello 
+echo "Remove '$module' module..."
+sudo /sbin/rmmod $module 
 echo "OK"
 
 # insert module
-sudo /sbin/insmod ./src/hello.ko major=$major minor=$minor || exit 1
+sudo /sbin/insmod ./src/$module.ko || exit 1
+
+# get version
+major=$(awk "\$2==\"$module\" {print \$1}" /proc/devices)
+minor=0
+
 
 # remove stale node, create a new one
-sudo rm -f /dev/hello 
-sudo mknod /dev/hello c $major $minor || exit 1
-echo "mknod /dev/hello c $major $minor"
+sudo rm -f /dev/$module 
+sudo mknod /dev/$module c $major $minor || exit 1
+echo "mknod /dev/$module c $major $minor"
 
 # change permissions
 group="staff"
 grep -q '^staff:' /etc/group || group="wheel"
-sudo chgrp $group /dev/hello
-sudo chmod 666 /dev/hello
+sudo chgrp $group /dev/$module
+sudo chmod 666 /dev/$module
 
 # show logging
 DATE=`date`
