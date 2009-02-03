622141aec7e44733be95529955749439665f2eef
hukka@mail.ru <hukka@mail.ru@65cfae95-534b-0410-ba89-db903692e17a>
2009-02-03 14:58:17 +0000

    dejaric unit tests created

---
+++ trunk/dejarik/test/dejarik_test.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/XmlOutputter.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/portability/Stream.h>
+
+
+using namespace std;
+
+class DejaricTest : public CppUnit::TestFixture 
+{
+   CPPUNIT_TEST_SUITE(DejaricTest);
+   CPPUNIT_TEST(testOne);
+   CPPUNIT_TEST_SUITE_END();
+         
+   
+public:         
+    void setUp() {}
+    void tearDown() {}
+        
+
+    void testOne() 
+    {
+        CPPUNIT_ASSERT(4 == 4);
+    }
+};
+
+
+CPPUNIT_TEST_SUITE_REGISTRATION(DejaricTest); 
+
+
+int main()
+{
+   CppUnit::Test *suite = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
+   CppUnit::TextUi::TestRunner runner;
+   runner.addTest(suite);
+   runner.setOutputter(new CppUnit::CompilerOutputter(&runner.result(), std::cout));
+   bool wasSuccessfull = runner.run();
+   if (!wasSuccessfull != 0) std::cout << "Error" << std::endl;
+   std::cout << "[OVAL] Cppunit-result ="<< !wasSuccessfull << std::endl;
+   return EXIT_SUCCESS;
+} 
