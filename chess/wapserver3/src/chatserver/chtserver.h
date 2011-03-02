cc1e56deeed9d2bd5e2bb9206fbf0059dc8b538d
hukka <hukka@mail.ru>
2011-03-02 09:05:15 +0000

    server: get chat history

--- chess/wapserver3/src/chatserver/chtserver.h
+++ chess/wapserver3/src/chatserver/chtserver.h
@@ -22,23 +22,26 @@ private:
 	~CHTServer();
 
 	void				DoAllMsg( MySocket* );
-    
+
     bool                checkParticipation( uint32_t _nPlayerID, uint32_t _nLogicID, uint32_t _nTableID = 0 );
-    
+
     bool                getGameOnlineUsersTable( uint32_t _nLogicID, SqlTable* _pRes );
     bool                getLogicChatTable( uint32_t _nLogicID, SqlTable* _pRes );
     bool                getBoardChatTable( uint32_t _nLogicID, SqlTable* _pRes );
-    
+
     void                newMsg( ClientMsg* _pMsg );
-        
+
     void                joinToChat( uint32_t _nPlayerID, uint32_t _nLogicID, uint32_t _nTableID = 0 );
     void                leaveChat( uint32_t _nPlayerID, uint32_t _nLogicID, uint32_t _nTableID = 0 );
-    void                messageToChat( uint32_t _nPlayerID, uint32_t _nLogicID, 
+    void                messageToChat( uint32_t _nPlayerID, uint32_t _nLogicID,
                                        const TVecChar* _vecData, uint32_t _nTableID = 0 );
     void                sendMsgToChat( uint32_t _nLogicID, CMyStr* _strMsg, uint32_t _nTableID = 0 );
     void                sendMsg( uint32_t _nTo, CSendedMsg *_pMsg );
     void                setSocket( MySocket * _pSocket );
+
 private:
+
+    void                getHistory( uint32_t logicID, uint32_t tableID);
 
 	static CHTServer*		m_pSelf;
 	static int			m_nRefCount;
