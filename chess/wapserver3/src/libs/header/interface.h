8fa29fcc68fe5f40ddc6a707bae2fe06da218b84
hukka <hukka@mail.ru>
2011-04-18 10:41:27 +0000

    server: Win renamed to CheckMate

--- chess/wapserver3/src/libs/header/interface.h
+++ chess/wapserver3/src/libs/header/interface.h
@@ -62,7 +62,7 @@ class IGameLogic
 
 public:
 
-	enum StepRes  { NotValid /*= 0*/,  Valid, Win, Surrender, Draw, TimeOut };
+	enum StepRes  { NotValid /*= 0*/,  Valid, CheckMate, Surrender, Draw, TimeOut };
 
 	virtual bool SetPos(const TVByte& _vecbtPos) = 0;		// Установить позицию на доске.
 
