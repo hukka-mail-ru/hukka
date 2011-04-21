b54c78f972edcacd653c968710b65517d59ce62c
hukka <hukka@mail.ru>
2011-04-21 11:30:41 +0000

    site: fixes

---
+++ chess/site1/enable_rating.php
@@ -0,0 +1,19 @@
+<html>
+ <head>
+  <title>Включить рейтинг</title>
+ </head>
+
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+
+ <body>
+
+<?php
+
+	printf("<h2>Включение рейтинга</h2>\n");	
+
+	printf("Пожалуйста, переведите 300руб. на ЯД.\n");	
+
+?> 
+
+</body>
+</html>
