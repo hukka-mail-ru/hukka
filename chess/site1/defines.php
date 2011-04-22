fa177abe2ebeaa7ddb7aa7ca35f507f099ee32a5
hukka <hukka@mail.ru>
2011-04-22 12:11:09 +0000

    site: ccs styles fixes

--- chess/site1/defines.php
+++ chess/site1/defines.php
@@ -9,5 +9,22 @@
 	$DbErrorSelect = 'Can\'t select DB (' . $DbName. '): ';
 
 
+	function OpenPage($name)
+	{
+		printf("<html>\n");
+		printf("<head>\n");
+		printf("<title>%s</title>\n", $name);
+		printf("<link rel='stylesheet' type='text/css' href='style/style.css' />\n");
+		printf("</head>\n");
+		printf("<meta http-equiv='content-type' content='text/html; charset=UTF-8'>\n");
+		printf("<body class='wrapper'>\n");
+	} 
+
+
+	function ClosePage()
+	{
+		printf("</body>\n");
+		printf("</html>\n");
+	} 
 
 ?> 
