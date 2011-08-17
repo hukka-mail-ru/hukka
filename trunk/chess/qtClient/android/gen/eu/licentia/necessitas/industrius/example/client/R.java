ee8887e422d0a6c281b130579b35b1aadd6a398a
hukka@mail.ru <hukka@mail.ru@65cfae95-534b-0410-ba89-db903692e17a>
2011-08-17 09:24:28 +0000

    fixes for android

--- trunk/chess/qtClient/android/gen/eu/licentia/necessitas/industrius/example/client/R.java
+++ trunk/chess/qtClient/android/gen/eu/licentia/necessitas/industrius/example/client/R.java
@@ -9,15 +9,18 @@ package eu.licentia.necessitas.industrius.example.client;
 
 public final class R {
     public static final class array {
-        public static final int bundled_libs=0x7f030001;
-        public static final int qt_libs=0x7f030000;
+        public static final int bundled_libs=0x7f040001;
+        public static final int qt_libs=0x7f040000;
     }
     public static final class attr {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class raw {
+        public static final int config=0x7f030000;
+    }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int app_name=0x7f050000;
     }
 }
