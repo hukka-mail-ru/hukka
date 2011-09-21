60a0c4b1e3c8e43ad85903dc0f4c34415860ed6b
hukka@mail.ru <hukka@mail.ru@65cfae95-534b-0410-ba89-db903692e17a>
2011-09-21 12:36:18 +0000

    server : No_square=100

--- trunk/chess/wapserver3/src/chessserver/engine/ChessTypes.h
+++ trunk/chess/wapserver3/src/chessserver/engine/ChessTypes.h
@@ -47,7 +47,7 @@ enum square_type {
     a6, b6, c6, d6, e6, f6, g6, h6,
     a7, b7, c7, d7, e7, f7, g7, h7,
     a8, b8, c8, d8, e8, f8, g8, h8,
-    Rank=8, File=1, No_square=-100
+    Rank=8, File=1, No_square=100
 };
 
 #pragma pack(1)
