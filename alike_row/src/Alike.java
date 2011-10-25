8fe9fbe4c7f5544a11ca3890c15d749d043b65a1
hukka <hukka@mail.ru>
2011-10-25 12:24:33 +0000

    alike fixes

--- alike_row/src/Alike.java
+++ alike_row/src/Alike.java
@@ -1,53 +1,172 @@
 
 import java.io.*;
-import java.util.regex.*;
+import java.util.Comparator;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.ArrayList;
 import java.lang.Exception;
 
 
-public class Alike {
+class Row
+{
+    public String name;
+    public ArrayList<Integer> numbers;
+    public int mismatches = 0;
+}
 
-    public static final Pattern pattern = Pattern.compile("\\w+\\s+[012]+\\D*");
-    
-    
 
-    private static void readFile(String fileName) 
+class DescSortMismatches implements Comparator<Row>
+{
+    public int compare(Row r1, Row r2)
     {
-        String thisLine;
-        
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(fileName));
-          
-            while ((thisLine = br.readLine()) != null) 
-            {
-              //  System.out.println(thisLine);
-                match(thisLine);
-            } 
-        } 
-        catch (IOException e) {
-          System.err.println("Ошибка: " + e);
+        int res = 0;
+
+        if(r1.mismatches < r2.mismatches)
+        {
+            res = 1;
         }
-        catch (Exception e) {
-            System.err.println("Ошибка: " + e);
+        else if(r1.mismatches > r2.mismatches)
+        {
+            res = -1;
         }
         
+        return res;
     }
-    
-	
-    private static void match(String str) throws Exception
-    {
-        String output = "Validation for " + str;
-        Matcher matcher = pattern.matcher(str);
-        if (!matcher.matches())
-            throw new Exception("неправильно сформирована строка: " + str);
-        
-        System.out.println(output);
-    }
-    
-    
+}
+
+
+
+public class Alike 
+{    
+    private ArrayList<Row> rows = new ArrayList<Row>();
+       
+   private void readFile(String fileName) 
+   {
+       Scanner scanner = null;
+               
+       try {
+           File file = new File(fileName);  
+           scanner = new Scanner(new FileReader(file));
+
+           while (scanner.hasNextLine()){
+               processLine( scanner.nextLine() );
+           }
+       }
+       catch (IOException e) {
+           System.err.println("Ошибка: " + e);
+           e.printStackTrace();
+       }
+       catch (Exception e)  {
+           System.err.println("Ошибка: " + e);
+           e.printStackTrace();
+       }
+       finally  {
+           scanner.close();
+       }
+   }
+   
+   private void processLine(String line) throws Exception
+   {
+       Scanner scanner = new Scanner(line);
+       scanner.useDelimiter("=");
+       if ( scanner.hasNext() )
+       {
+           String name = scanner.next().trim();
+           String value = scanner.next().trim();
+        //   System.out.println("Name is : " + name.trim() + ", and Value is : " + value.trim());
+
+           Row row = new Row();
+           row.name = name;
+           
+           ArrayList<Integer> numbers = new ArrayList<Integer>();
+           for(int i=0; i<value.length(); i++)
+           {
+               Character c = new Character(value.charAt(i));
+               String s = c.toString();
+               numbers.add(Integer.parseInt(s));
+           }
+           
+           row.numbers = numbers;
+                   
+           rows.add(row);
+       }
+       else 
+       {
+           throw new Exception("пустой или неправильный файл.");
+       }
+   }
+   
+   private void checkLength() throws Exception
+   {
+       for(int row=0; row<rows.size(); row++)
+       {
+           for(int i=row+1; i<rows.size(); i++)
+           {
+               if(rows.get(row).numbers.size() != rows.get(i).numbers.size())
+               {
+                   throw new Exception("разная длина данных в рядах " + rows.get(row).name + " и " + rows.get(i).name);
+               }
+           }
+       }
+   }
+   
+   private void compareRows()
+   {
+       for(int row=0; row<rows.size(); row++)
+       {
+           Row currentRow = rows.get(row);
+           for(int num=0; num<currentRow.numbers.size(); num++)
+           {
+               for(int compared=row+1; compared<rows.size(); compared++)
+               {
+                   Row comparedRow = rows.get(compared);
+                   
+                   if(currentRow.numbers.get(num) != comparedRow.numbers.get(num))
+                   {
+                       currentRow.mismatches++;
+                       comparedRow.mismatches++;
+                   }
+               }
+           }
+       }
+   }
+   
+
+   
+   private void output()
+   {
+       Collections.sort(rows, new DescSortMismatches());  
+       
+       for(int i=0; i<rows.size(); i++)
+       {
+           System.out.println(rows.get(i).name + ": отличий:  " +  rows.get(i).mismatches);
+           
+         /*  ArrayList<Integer> numbers = rows.get(i).numbers;
+           for(int j=0; j<numbers.size(); j++)
+           {
+               System.out.println("val is : " + numbers.get(j));
+           }*/
+       }
+   }
+   
+   
+    // constuctor
+    public Alike() {}
 	
     public static void main(String[] args) 
     {
-        System.out.println("Здравствуй, мир!");  
-        readFile(args[0]);
+        Alike alike = new Alike();
+        try
+        {
+            alike.readFile(args[0]);
+            alike.checkLength();
+            alike.compareRows();
+            alike.output();
+        }
+        catch (Exception e)  {
+            System.err.println("Ошибка: " + e);
+            e.printStackTrace();
+        }
+        
 	}
 }
