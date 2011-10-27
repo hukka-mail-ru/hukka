0b82f0f919938618ea510d5e823fe1e04e677c65
hukka@mail.ru <hukka@mail.ru@65cfae95-534b-0410-ba89-db903692e17a>
2011-10-27 12:37:14 +0000

    JabberClient: launched on android

--- trunk/jabber/src/ru/hukka/jabber/JabberClientActivity.java
+++ trunk/jabber/src/ru/hukka/jabber/JabberClientActivity.java
@@ -3,7 +3,7 @@ package ru.hukka.jabber;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
-
+import org.jivesoftware.smack.*;
 
 public class JabberClientActivity extends Activity {
     /** Called when the activity is first created. */
@@ -14,5 +14,39 @@ public class JabberClientActivity extends Activity {
         TextView tv = new TextView(this);
         tv.setText("Hello, Android");
         setContentView(tv);
+        
+        // Create a connection to the igniterealtime.org XMPP server.
+        try
+        {
+            Connection con = new XMPPConnection("igniterealtime.org");
+            // Connect to the server
+            con.connect();
+            // Most servers require you to login before performing other tasks.
+            con.login("jsmith", "mypass");
+        }
+        catch (XMPPException e)
+        {
+            tv.setText("Error: " + e.getMessage());
+            setContentView(tv);
+            
+            System.out.println("Error: " + e.getMessage()); 
+            e.printStackTrace();
+        }
+            
+            
+            
+        // Start a new conversation with John Doe and send him a message.
+        /*
+        Chat chat = connection.getChatManager().createChat("jdoe@igniterealtime.org", new MessageListener() {
+        
+
+       public void processMessage(Chat chat, Message message) { 
+           // Print out any messages we get back to standard out. 
+           System.out.println("Received message: " + message); 
+        } }); 
+        
+        chat.sendMessage("Howdy!"); // Disconnect from the server con.disconnect();
+        */ 
+       //}
     }
 }
