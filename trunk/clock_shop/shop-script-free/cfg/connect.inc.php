5f46dfc858c0b25af95c8a3efeb4814ea130dc50
hukka.mail.ru@gmail.com <hukka.mail.ru@gmail.com@65cfae95-534b-0410-ba89-db903692e17a>
2012-01-23 09:32:31 +0000

    some pages recoded

--- trunk/clock_shop/shop-script-free/cfg/connect.inc.php
+++ trunk/clock_shop/shop-script-free/cfg/connect.inc.php
@@ -2,11 +2,11 @@
 	//database connection settings
 
 	define('DB_HOST', 'localhost'); // database host
-	define('DB_USER', ''); // username
-	define('DB_PASS', ''); // password
-	define('DB_NAME', ''); // database name
-	define('ADMIN_LOGIN', ''); //administrator's login
-	define('ADMIN_PASS', ''); //administrator's login
+	define('DB_USER', 'hukka'); // username
+	define('DB_PASS', '777'); // password
+	define('DB_NAME', 'ClockShop'); // database name
+	define('ADMIN_LOGIN', 'YWRtaW4='); //administrator's login
+	define('ADMIN_PASS', 'f1c1592588411002af340cbaedd6fc33'); //administrator's login
 
 	//database tables
 	include("./cfg/tables.inc.php");
