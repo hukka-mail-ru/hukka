b39dd217e00748f64a376269aa40b5dc7c27e565
hukka.mail.ru@gmail.com <hukka.mail.ru@gmail.com@65cfae95-534b-0410-ba89-db903692e17a>
2012-02-14 12:42:29 +0000

    prices added

--- trunk/clock_shop/scripts/insert.pl
+++ trunk/clock_shop/scripts/insert.pl
@@ -9,15 +9,16 @@ while ($line = <>) # read a file
   #  print "a new line\n";
 
 
-    if($line =~ m/([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+/)
+    if($line =~ m/([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+/)
     {
 	$code         = $1;
 	$size         = $2;
 	$categoryID   = $3;
 	$price        = $4;
-	$stock        = $5;
-	$name         = $6;
-	$color        = $7;
+	$stock_price  = $5;
+	$stock        = $6;
+	$name         = $7;
+	$color        = $8;
 	
 	$color = ($color =~ "NA") ? "" : "Цвет: $color. ";
 
@@ -26,13 +27,13 @@ while ($line = <>) # read a file
 	{
 		print "INSERT INTO ClockShop.SS_products (product_code, categoryID, name, brief_description, description, 
 		       enabled, customers_rating, customer_votes, in_stock, items_sold,
-		       Price, list_price,
+		       Price, list_price, stock_price,
 		       thumbnail, picture, big_picture)
 		       VALUES (
 		       '$code', $categoryID, '$name', 'Размер: $size', 
 	               'Керамические часы ручной работы. Бесшумный механизм. Питание от двух батареек АА. $colorРазмер: $size',
 		       1, 0, 0, 1, 0,
-		       $price, $price,
+		       $price, $price, $stock_price,
 		       'thumbnail/$code.jpg', 'small/$code.jpg', 'big/$code.jpg'); \n\n"
 	}
     }
--- trunk/clock_shop/scripts/insert_categories.pl
+++ trunk/clock_shop/scripts/insert_categories.pl
@@ -9,11 +9,18 @@ while ($line = <>) # read a file
   #  print "a new line\n";
 
 
-    if($line =~ m/([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+/)
+    if($line =~ m/([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+/)
     {
+	$categoryID  = $1;
+	$parent      = $2;
+	$name        = $3;
+	$name_de     = $4;
+	$picture_code = $5;
+	$description = $6;
+	
 
-	print "INSERT INTO ClockShop.SS_categories (categoryID, parent, name, picture, description, products_count, products_count_admin)
-	        VALUES ( $1, $2, '$3', 'category/$4.jpg', '$5', 0, 0); \n\n";
+	print "INSERT INTO ClockShop.SS_categories (categoryID, parent, name, name_de, picture, description, products_count, products_count_admin)
+	        VALUES ( $categoryID, $parent, '$name', '$name_de', 'category/$picture_code.jpg', '$description', 0, 0); \n\n";
     }
 
 
