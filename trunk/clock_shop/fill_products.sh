553eeda0e89ef15b5493467db9619eb94cb3dd7b
hukka.mail.ru@gmail.com <hukka.mail.ru@gmail.com@65cfae95-534b-0410-ba89-db903692e17a>
2012-02-14 08:58:35 +0000

    insert script

---
+++ trunk/clock_shop/fill_products.sh
@@ -0,0 +1,4 @@
+perl insert.pl artikul.txt > insert.sql
+
+mysql -u root -proot < insert.sql
+
---
+++ trunk/clock_shop/insert.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+# Usage : perl insert.pl artikul.txt
+
+print "DELETE FROM ClockShop.SS_products;\n\n";
+
+while ($line = <>) # read a file
+{ 
+  #  print "a new line\n";
+
+
+    if($line =~ m/([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+([^\t]+)\t+/)
+    {
+
+	print "INSERT INTO ClockShop.SS_products (product_code, categoryID, name, brief_description, description, 
+	       enabled, customers_rating, customer_votes, in_stock, items_sold,
+	       Price, list_price,
+	       thumbnail, picture, big_picture)
+	       VALUES (
+	       '$1', $4, '$6', 'brief_description', 'description',
+	       1, 0, 0, 1, 0,
+	       1999, 1999,
+	       'thumbnail/$1.jpg', 'small/$1.jpg', 'big/$1.jpg'); \n\n"
+    }
+
+
+}
