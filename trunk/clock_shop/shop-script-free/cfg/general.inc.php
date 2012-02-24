bdd8645ac423ba4ebd9cb2bbb3a249f3baaa9341
hukka.mail.ru@gmail.com <hukka.mail.ru@gmail.com@65cfae95-534b-0410-ba89-db903692e17a>
2012-02-24 08:00:21 +0000

    general info fixed

--- trunk/clock_shop/shop-script-free/cfg/general.inc.php
+++ trunk/clock_shop/shop-script-free/cfg/general.inc.php
@@ -1,8 +1,8 @@
 <?php
 	define('CONF_SHOP_NAME', 'Chasiki.com.ru');
-	define('CONF_SHOP_URL', 'www.mystore.ru');
-	define('CONF_GENERAL_EMAIL', 'your@email.ru');
-	define('CONF_ORDERS_EMAIL', 'your@email.ru');
+	define('CONF_SHOP_URL', 'http://www.chasiki.com.ru');
+	define('CONF_GENERAL_EMAIL', 'support@chasiki.com.ru');
+	define('CONF_ORDERS_EMAIL', 'support@chasiki.com.ru');
 	define('CONF_CURRENCY_ID_LEFT', '');
 	define('CONF_CURRENCY_ID_RIGHT', ' руб.');
 	define('CONF_CURRENCY_ISO3', 'RUR');
