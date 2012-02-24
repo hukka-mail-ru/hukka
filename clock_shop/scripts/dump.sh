d93b4801703c5eae8ae2f3b4bcd25f7e89c5d345
hukka.mail.ru <hukka.mail.ru@gmail.com>
2012-02-24 07:24:43 +0000

    scripts fixed

--- clock_shop/scripts/dump.sh
+++ clock_shop/scripts/dump.sh
@@ -1,2 +1,6 @@
 mysqldump -u root -proot --database ClockShop > dump.sql
+
+perl -i -p -e 's/CREATE DATABASE/\-\- CREATE DATABASE/g' dump.sql
+perl -i -p -e 's/USE \`ClockShop\`;/USE \`db67829m\`;/g' dump.sql
+
 cp dump.sql ~/temp
