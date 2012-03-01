3ab530371239c71a32428c55b67ca946e96c5616
hukka.mail.ru@gmail.com <hukka.mail.ru@gmail.com@65cfae95-534b-0410-ba89-db903692e17a>
2012-03-01 08:13:48 +0000

    misc fixes

--- trunk/clock_shop/scripts/fill_products.sh
+++ trunk/clock_shop/scripts/fill_products.sh
@@ -3,5 +3,7 @@ perl insert_products.pl artikul.txt > temp.sql
 
 mysql -u root -proot --default-character-set=utf8 < temp.sql
 
+./change_offers.sh
+
 # rm temp.sql
 # rm dump.sql
---
+++ trunk/clock_shop/scripts/show_discounts.php
@@ -0,0 +1,25 @@
+<?php
+
+	//connect to the database
+	mysql_connect("localhost", "hukka", "777") or die (db_error());
+	mysql_select_db("ClockShop") or die (db_error());
+
+
+
+	$q = mysql_query("SELECT price, list_price, productID, name, product_code FROM SS_products") or die (db_error());
+	while ($row = mysql_fetch_row($q))
+	{
+		$price = $row[0];
+		$list_price = $row[1];
+		$productID = $row[2];
+		$name = $row[3];
+		$product_code = $row[4];
+
+		if($price != $list_price)
+		{
+			$discount = $list_price - $price;
+			printf("$productID\t$price\t$list_price\t($discount)\t$product_code\t$name\n");
+		}
+	}
+
+?>
