b4841273729483a2ac331950e8b943cfcd2d96d8
hukka.mail.ru <hukka.mail.ru@gmail.com>
2012-03-22 07:59:16 +0000

    keywords

--- clock_shop/shop-script-free/cfg/general.inc.php
+++ clock_shop/shop-script-free/cfg/general.inc.php
@@ -1,5 +1,7 @@
 <?php
 	define('CONF_SHOP_NAME', 'Chasiki.com.ru');
+	define('CONF_SHOP_TITLE', 'Часы, настенные часы, подарки, сувениры. Интернет магазин часов Chasiki.com.ru');
+	define('CONF_SHOP_META', 'Часы, необычные подарки, интерьер, настенные часы. Интернет магазин часов Chasiki.com.ru');
 	define('CONF_SHOP_URL', 'http://www.chasiki.com.ru');
 	define('CONF_SHOP_EMAIL', 'support@chasiki.com.ru');
 	define('CONF_CURRENCY_ID_LEFT', '');
