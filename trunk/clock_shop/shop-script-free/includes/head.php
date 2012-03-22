87b456aa10882afac44bfbfec970cb116ea61859
hukka.mail.ru@gmail.com <hukka.mail.ru@gmail.com@65cfae95-534b-0410-ba89-db903692e17a>
2012-03-22 07:59:16 +0000

    keywords

--- trunk/clock_shop/shop-script-free/includes/head.php
+++ trunk/clock_shop/shop-script-free/includes/head.php
@@ -20,15 +20,15 @@
 	{
 		$q = db_query("SELECT ".$name." FROM ".CATEGORIES_TABLE." WHERE categoryID<>0 and categoryID='$categoryID'") or die (db_error());
 		$r = db_fetch_row($q);
-		$page_title = str_replace("\"","'",$r[0]." - ".CONF_SHOP_NAME);
+		$page_title = str_replace("\"","'",$r[0]." - ".CONF_SHOP_TITLE);
 	}
 	else if (isset($productID) && $productID>0)
 	{
 		$q = db_query("SELECT ".$name." FROM ".PRODUCTS_TABLE." WHERE productID='$productID'") or die (db_error());
 		$r = db_fetch_row($q);
-		$page_title = str_replace("\"","'",$r[0]." - ".CONF_SHOP_NAME);
+		$page_title = str_replace("\"","'",$r[0]." - ".CONF_SHOP_TITLE);
 	}
-	else $page_title = CONF_SHOP_NAME;
+	else $page_title = CONF_SHOP_TITLE;
 
 
 	// META
@@ -45,7 +45,7 @@
 		$r = db_fetch_row($q);
 		$page_meta = str_replace("\"","'",$r[0].", ".$r[1]);
 	}
-	else $page_meta = CONF_SHOP_NAME.", powered by Shop-Script";
+	else $page_meta = CONF_SHOP_META;
 
 
 
