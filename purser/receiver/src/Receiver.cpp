59049abe87230a07bf034141df3d341863cba52d
hukka.mail.ru <hukka.mail.ru@gmail.com>
2013-02-05 11:14:15 +0000

    purser: no main.cpp

--- purser/receiver/src/Receiver.cpp
+++ purser/receiver/src/Receiver.cpp
@@ -6,7 +6,7 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-
+#include <iostream>
 
 #include "Receiver.h"
 #include "MyException.h"
@@ -63,3 +63,49 @@ void Receiver::ListenPort(int port)
 }
 
 
+int main(int argc, char** argv)
+{
+	try
+	{
+		string logfile = "/var/log/receiver.log";
+
+		// READ COMMAND LINE
+		string pidfile = "/var/run/receiver.pid";
+		int port = 1234;
+
+		for (int i = 0; i < argc; i++)
+		{
+			string arg = argv[i];
+
+			if (arg == "--pidfile" && i+1 < argc)
+			{
+				pidfile = argv[i+1];
+			}
+			if (arg == "--logfile" && i+1 < argc)
+			{
+				logfile = argv[i+1];
+			}
+			else if (arg == "--port" && i+1 < argc)
+			{
+				port = atoi(argv[i+1]);
+			}
+		}
+
+		Log::SetLogFile(logfile);
+
+		Receiver receiver(pidfile);
+
+		receiver.ListenPort(port);
+
+		cout << "Log: " << logfile << endl;
+		cout << "Ready - port " << port << endl;
+
+		// daemon loop
+		return receiver.Daemonize();
+	}
+	catch (MyException& e)
+	{
+		cout << e.what() << endl;
+		return -1;
+	}
+}
