c6b7a1adc511b85790821f801eb654ac64af0614
hukka.mail.ru <hukka.mail.ru@gmail.com>
2013-02-25 12:15:25 +0000

    purser: logging

--- purser/ui/HelloWorld/src/com/example/helloworld/Log.java
+++ purser/ui/HelloWorld/src/com/example/helloworld/Log.java
@@ -1,11 +1,27 @@
 package com.example.helloworld;
 
+import javax.microedition.lcdui.*;
+
 public class Log 
 {
+	public static void write(String str)
+	{
+		 System.out.println(str);
+	}
+	
 	public static void write(Exception e)
 	{
 		 System.out.println("Exception!");
 		 System.out.println(e.getMessage());
 		 e.printStackTrace(); 
+	}
+
+	public static void show(Display display, Form form, Exception e)
+	{	
+		Log.write(e);
+		
+		Alert alert = new Alert("Error");
+		alert.setString(e.getMessage());
+		display.setCurrent(alert, form);
 	}
 }
