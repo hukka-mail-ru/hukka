8d4e92cc6c3b44695e7b424486293d99ae73e3a0
hukka.mail.ru@gmail.com <hukka.mail.ru@gmail.com@65cfae95-534b-0410-ba89-db903692e17a>
2013-03-04 07:42:57 +0000

    purser: rename dir

--- trunk/purser/ui/HelloWorld/src/com/tsi/purser/exceptions/NoDataException.java
+++
@@ -1,9 +0,0 @@
-package com.tsi.purser.exceptions;
-
-public class NoDataException extends Exception
-{
-	public NoDataException(String missingData)
-	{
-		super("Please provide " + missingData);
-	}
-}
--- trunk/purser/ui/HelloWorld/src/com/tsi/purser/forms/Widget.java
+++
@@ -1,9 +0,0 @@
-package com.tsi.purser.forms;
-
-import javax.microedition.lcdui.*;
-
-public interface Widget 
-{
-    public Displayable getWidget();
-    public Item getDefaultItem();
-}
