70f5d0d7fa5a0a33967c290acb0bdcfe07abaaca
hukka.mail.ru <hukka.mail.ru@gmail.com>
2013-03-04 07:42:57 +0000

    purser: rename dir

--- purser/ui/HelloWorld/src/com/tsi/purser/exceptions/InvalidSymbolException.java
+++
@@ -1,9 +0,0 @@
-package com.tsi.purser.exceptions;
-
-public class InvalidSymbolException extends Exception
-{
-	public InvalidSymbolException(String where)
-	{
-		super("Invalid symbol(s) in '" + where + "' field");
-	}
-}
