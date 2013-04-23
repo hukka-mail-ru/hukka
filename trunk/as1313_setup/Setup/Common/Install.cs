6beadff26fa2db484257b58f0e65f394b190fedc
hukka.mail.ru@gmail.com <hukka.mail.ru@gmail.com@65cfae95-534b-0410-ba89-db903692e17a>
2013-04-23 11:02:24 +0000

    Setup: run SQl script

--- trunk/as1313_setup/Setup/Common/Install.cs
+++ trunk/as1313_setup/Setup/Common/Install.cs
@@ -18,6 +18,8 @@ namespace Setup.Common
     /// </summary>
     public class Install
     {
+        private static string mVersionDir;
+
         public static void CreateFolders()
         {
             // company dir
@@ -36,28 +38,44 @@ namespace Setup.Common
 
             // version dir
             FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(
-                     Path.Combine(Settings.ClientDir, Settings.ClientExecutable));
+                     Path.Combine(Settings.SourceDir, Settings.SourceExecutable));
 
-            string versionDir = Path.Combine(productDir, fvi.FileVersion);
+            mVersionDir = Path.Combine(productDir, fvi.FileVersion);
 
-            if (!Directory.Exists(versionDir))
+            if (!Directory.Exists(mVersionDir))
             {
-                Directory.CreateDirectory(versionDir);
+                Directory.CreateDirectory(mVersionDir);
             }
         }
 
 
-
-
         public static void CopyFiles()
         {
-
+            CopyDirectory(Settings.SourceDir, mVersionDir);
         }
 
 
         public static void RunSqlScript()
         {
+            string scriptsDir = Path.Combine(Directory.GetCurrentDirectory(), Settings.SourceDir, Settings.ScriptsDir);
+            string script = Path.Combine(scriptsDir, "databaseAs1313.sql");
+            string outputFile = Path.Combine(Directory.GetCurrentDirectory(), "RunSQLScriptOutput.txt");
+            string cmd = "sqlcmd.exe";
 
+            string args = 
+                    " -S " + Settings.SQLServer + 
+                    " -i \"" + script + "\"" +
+                    " -v DirParam=\"" + scriptsDir + "\\\"" +
+                    " -U " + Settings.SQLUser +
+                    " -P " + Settings.SQLPassword + 
+                    " -o " + outputFile;
+    
+            Process ExternalProcess = new Process();
+            ExternalProcess.StartInfo.FileName = cmd;
+            ExternalProcess.StartInfo.Arguments = args;
+            ExternalProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            ExternalProcess.Start();
+            ExternalProcess.WaitForExit();        
         }
 
 
@@ -70,6 +88,42 @@ namespace Setup.Common
         public static void Rollback()
         {
 
+        }
+
+
+        private class Folders
+        {
+            public string Source { get; private set; }
+            public string Target { get; private set; }
+
+            public Folders(string source, string target)
+            {
+                Source = source;
+                Target = target;
+            }
+        }
+
+        private static void CopyDirectory(string source, string target)
+        {
+            var stack = new Stack<Folders>();
+            stack.Push(new Folders(source, target));
+
+            while (stack.Count > 0)
+            {
+                var folders = stack.Pop();
+                Directory.CreateDirectory(folders.Target);
+                foreach (var file in Directory.GetFiles(folders.Source, "*.*"))
+                {
+                    string targetFile = Path.Combine(folders.Target, Path.GetFileName(file));
+                    if (File.Exists(targetFile)) File.Delete(targetFile);
+                    File.Copy(file, targetFile);
+                }
+
+                foreach (var folder in Directory.GetDirectories(folders.Source))
+                {
+                    stack.Push(new Folders(folder, Path.Combine(folders.Target, Path.GetFileName(folder))));
+                }
+            }
         }
     }
 }
